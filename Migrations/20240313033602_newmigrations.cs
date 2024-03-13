a312d6febcb67acfd53a2e6078bc6f472a10c453
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2024-03-12 23:36:19 -0400

    adiciona migrations novas

---
+++ Migrations/20240313033602_newmigrations.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parking.Migrations
+{
+    /// <inheritdoc />
+    public partial class newmigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PricePerHourInCents",
+                table: "Parks",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PaidValueInCents",
+                table: "Events",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "PricePerHourInCents",
+                table: "Parks",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "PaidValueInCents",
+                table: "Events",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+    }
+}
