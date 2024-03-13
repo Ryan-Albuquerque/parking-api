39fe8074b5f16114f3ef16b8b6c37d8d87483584
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2024-03-12 21:22:11 -0400

    Create ResponseHandle.cs

---
+++ Utils/ResponseHandle.cs
@@ -0,0 +1,8 @@
+﻿namespace Parking.Utils
+{
+    public class ResponseHandler<T>(T? result, string? error)
+    {
+        public T? Result { get; } = result;
+        public string? Error { get; } = error;
+    }
+}
